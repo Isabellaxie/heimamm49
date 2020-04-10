fa70fff94139390ebfdad15010d00bb69fbef844
Isabellaxie <58657425+Isabellaxie@users.noreply.github.com>
2020-04-10 08:48:25 +0800

    创建layout成功

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,9 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
-// import heima_user from '../components/heima_user'
+
 import login from '../view/login/login.vue'
-// import heima_city from '../components/heima_city'
-// import heima_direction from '../components/heima_direction'
-// import heima_enterprise from '../components/heima_enterprise'
-// import heima_options from '../components/heima_options'
-// import heima_questions from '../components/heima_questions'
-// import heima_role from '../components/heima_role'
-// import heima_subject from '../components/heima_subject'
+import layout from '../view/home/layout.vue'
+
 const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
     return originalPush.call(this, location).catch(err => err)
@@ -25,30 +20,11 @@ const router = new VueRouter({
             component: login
         },
 
-        // {
-        //     path: "/heima_city",
-        //     component: heima_city
-        // },
-        // {
-        //     path: "/heima_direction",
-        //     component: heima_direction
-        // },
-        // {
-        //     path: "/heima_enterprise",
-        //     component: heima_enterprise
-        // },
-        // {
-        //     path: "/heima_options",
-        //     component: heima_options
-        // },
-        // {
-        //     path: "/heima_questions",
-        //     component: heima_questions
-        // },
-        // {
-        //     path: "/heima_role",
-        //     component: heima_role
-        // },
+        {
+            path: "/home",
+            component: layout
+        },
+
         // {
         //     path: "/heima_subject",
         //     component: heima_subject
